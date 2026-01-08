0333333289a9163efa6a83bd169bc6a24ffe2adf
Shravya_Oruganti <shravyaoruganti2004@gmail.com>
2026-01-08 22:51:37 +0530

    Add files via upload
    datatypes ,variables ,IO functions

---
+++ Day 2-datatypes,variables,IO Functions.py
@@ -0,0 +1,16 @@
+#datatypes,variables and IO functions
+name="shravya" #string type
+age=21 #integer
+weight=50.5 #float datatype
+list1=[1,2,3,1,2,'hi',2.4,(10,20)] #list of items
+tuple1=(10,20,30,'welcome','hello') #Tuple of items
+cards={'A',2,3,4,5,6,7,8,9,10,'J','Q','K',2,3,4} # Pack of cards in a set datatype
+student_marks={'maths':90,"telugu":85,"english":75} #dictionary datatype
+print("My name is:",name)
+print("My age is",age)
+print("My weight is:",weight)
+print("List of items:",list1)
+print("tuple of items:",tuple1)
+print("pack of card values:",cards)
+print("student subject wise marks:",student_marks)
+      
